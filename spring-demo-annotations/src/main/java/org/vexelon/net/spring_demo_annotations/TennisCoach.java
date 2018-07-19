391052cbdf16370a68e426d40fc1ce03e121c565
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2018-07-19 22:37:48 +0300

    Added Setter DI with Java Annotation

--- spring-demo-annotations/src/main/java/org/vexelon/net/spring_demo_annotations/TennisCoach.java
+++ spring-demo-annotations/src/main/java/org/vexelon/net/spring_demo_annotations/TennisCoach.java
@@ -8,17 +8,30 @@ public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	public TennisCoach() {
+		System.out.println(">> Tennis Coach: inside default construcotr");
+	}
 	
+	// Constructor injection
+//	@Autowired
+//	public TennisCoach(FortuneService theFortuneService) {
+//		super();
+//		this.fortuneService = theFortuneService;
+//	}
+
+
+	// Setter injection
 	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
-		super();
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">> Tennis Coach: inside setFortuneService() method");
 		this.fortuneService = theFortuneService;
 	}
-
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
 	}
+
 
 	@Override
 	public String getDailyFortune() {
