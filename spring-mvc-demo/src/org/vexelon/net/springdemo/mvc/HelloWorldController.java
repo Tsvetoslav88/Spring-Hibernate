f119baaeacfbfbb367ea8ddcc34b8eb81c6dc00d
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2018-08-16 12:38:05 +0300

    Add @RequestParam in

--- spring-mvc-demo/src/org/vexelon/net/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/org/vexelon/net/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -42,4 +43,19 @@ public class HelloWorldController {
 		return "helloworld";
 	}
 	
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+		
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		//create a message
+		String result = "Hey my friend from v3 " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
 }
