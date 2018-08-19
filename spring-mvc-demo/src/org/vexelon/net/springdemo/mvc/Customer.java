44e60d2c1426427c3a6f51298a5853e67e6308c4
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2018-08-19 14:48:36 +0300

    Add @InitBinder for Trimming Whitespaces

--- spring-mvc-demo/src/org/vexelon/net/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/org/vexelon/net/springdemo/mvc/Customer.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.Size;
 public class Customer {
 	private String firstName;
 	
-	@NotNull
+	@NotNull(message="is required")
 	@Size(min=1, message="is required")
 	private String lastName;
 	
--- spring-mvc-demo/src/org/vexelon/net/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/org/vexelon/net/springdemo/mvc/CustomerController.java
@@ -2,15 +2,27 @@ package org.vexelon.net.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	// add an initbinder...to convert trim  input strings
+	// remove leading and trailing whitespace
+	// resolved issue for our validation
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
@@ -27,4 +39,5 @@ public class CustomerController {
 			return "customer-confirmation";
 		}
 	}
+	
 }
