554a5f21d89d2727a920b2c32c63a16ab1a58676
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2018-08-21 22:47:25 +0300

    Create Hibernate project with JDBC test connection

---
+++ hibernate-tutorial/src/org/vexelon/net/jdbc/TestJdbc.java
@@ -0,0 +1,22 @@
+package org.vexelon.net.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String user = "hbstudent";
+		String password = "mysql";
+		try {
+			System.out.println("Connecting to DB: " + jdbcUrl);
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, password);
+			System.out.println("Connection successful!!!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
