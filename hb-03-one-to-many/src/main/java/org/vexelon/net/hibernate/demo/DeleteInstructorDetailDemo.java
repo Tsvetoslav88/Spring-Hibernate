8f7bfe304a30ec8574cc3d690d4275ccdb5637cf
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2018-09-02 23:50:54 +0300

    Add Course with @Meny-to-one relation with Instructor

---
+++ hb-03-one-to-many/src/main/java/org/vexelon/net/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,59 @@
+package org.vexelon.net.hibernate.demo;
+
+import java.text.ParseException;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.vexelon.net.hibernate.demo.entity.Instructor;
+import org.vexelon.net.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) throws ParseException {
+		// create SessionFactory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).buildSessionFactory();
+
+		// create Session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+
+			// get the instructor detail object
+			int theId = 3;
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+
+			// print the instructor detail
+			System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+
+			// print the associated instructor
+			System.out.println("the associated instructor: " + tempInstructorDetail.getInstructor());
+
+			// not let's delete the instructor details
+			
+			//remove the associated object referance
+			//break bi-directional referance
+			tempInstructorDetail.getInstructor().setInstructorDetail(null);
+			System.out.println("Deleting tempInstructorDetail: " + tempInstructorDetail);
+			session.delete(tempInstructorDetail);
+			
+			// commit transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		} finally {
+			// handle connection leak issue
+			session.close();
+
+			factory.close();
+		}
+
+	}
+
+}
