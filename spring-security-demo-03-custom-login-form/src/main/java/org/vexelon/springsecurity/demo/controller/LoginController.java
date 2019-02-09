d1587ec9c3d479075ee02c9ca359ccf0e5a1fdd8
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2019-02-09 10:19:38 +0200

    Create Custom Login Form

---
+++ spring-security-demo-03-custom-login-form/src/main/java/org/vexelon/springsecurity/demo/controller/LoginController.java
@@ -0,0 +1,14 @@
+package org.vexelon.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginPage() {
+		return "plain-login";
+	}
+	
+}
---
+++ spring-security-demo-03-custom-login-form/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>Vexelon Home Page</title>
+</head>
+<body>
+	<h2>Vexelon Home Page</h2>
+
+	Welcome to Vexelon Home Page
+
+</body>
+</html>
