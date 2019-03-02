9d10b2c9064cac6679b52ea41b5c3a41555f9491
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2019-03-02 13:59:19 +0200

    Add getStudent by index

--- spring-rest-demo/src/main/java/com/vexelon/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/vexelon/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.vexelon.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +16,29 @@ import com.vexelon.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
+	List<Student> theStudents;
+	
+	// define @PostConstruct to load the students data..only once
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<>();
+		theStudents.add(new Student("Ivan", "Ivanov"));
+		theStudents.add(new Student("Petar", "Petrov"));
+		theStudents.add(new Student("Minko", "Minkov"));
+	}
+	
+	
 	// define endpoint for "/student"
 	@GetMapping("/students")
 	public List<Student> getStudents(){
-		List<Student> students = new ArrayList<>();
-		students.add(new Student("Ivan", "Ivanov"));
-		students.add(new Student("Petar", "Petrov"));
-		students.add(new Student("Minko", "Minkov"));
 		
-		return students;
+		
+		return theStudents;
 	}
 	
+	// define endpoint for "/students/{studentId}"
+	@GetMapping("students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		return theStudents.get(studentId);
+	}
 }
