e55be6d9548979f32ac148811912b4fea4c6fd4a
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2019-03-02 12:52:47 +0200

    Create Spring-rest demo project and remove .DS_Store files from git track

---
+++ spring-rest-demo/src/main/java/com/vexelon/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,14 @@
+package com.vexelon.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Controller
+@EnableWebMvc
+@ComponentScan("com.vexelon.springdemo")
+public class DemoAppConfig {
+
+	
+	
+}
