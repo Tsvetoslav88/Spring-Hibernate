f6833a623b34c7f9a3cc17f89be4253336450426
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2019-03-03 11:25:18 +0200

    Add StudentControler Bad request handler

--- spring-rest-demo/src/main/java/com/vexelon/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/vexelon/springdemo/rest/StudentRestController.java
@@ -66,4 +66,21 @@ public class StudentRestController {
 		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
 		
 	}
+
+	// add another exception handler ... to catch any exception
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handlerExpcetion(Exception exc){
+		
+		// create a StudentErrorResponse
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		
+		// return ResponseEntity
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+		
+	}
+
 }
