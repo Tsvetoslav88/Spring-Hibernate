c0407d349707c3d179e5fa85c22d1c2d735f4c48
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2019-05-04 18:18:38 +0300

    Create Spring boot Actuator demo

---
+++ 03-spring-boot-actuator-demo/src/main/java/org/vexelon/springbootdemo/springbootapp/SpringbootappApplication.java
@@ -0,0 +1,13 @@
+package org.vexelon.springbootdemo.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootappApplication.class, args);
+	}
+
+}
