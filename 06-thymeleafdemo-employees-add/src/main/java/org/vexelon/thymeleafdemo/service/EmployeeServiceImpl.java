3f47014c3baa2cf3dfe841f05b6e34b7b9ab589b
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2019-05-12 18:00:58 +0300

    Create Thymeleafe demo for adding of employee

---
+++ 06-thymeleafdemo-employees-add/src/main/java/org/vexelon/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,50 @@
+package org.vexelon.thymeleafdemo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.vexelon.thymeleafdemo.dao.EmployeeRepository;
+import org.vexelon.thymeleafdemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	private EmployeeRepository employeeRepository;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+		this.employeeRepository = theEmployeeRepository;
+	}
+	
+	@Override
+	public List<Employee> findAll() {
+		return employeeRepository.findAllByOrderByLastNameAsc();
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		Optional<Employee> result = employeeRepository.findById(theId);
+		
+		Employee theEmployee = null;
+		if(result.isPresent()) {
+			theEmployee = result.get();
+		}else {
+			throw new RuntimeException("Did not find employee id: " + theId);
+		}
+		
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		employeeRepository.save(theEmployee);
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		employeeRepository.deleteById(theId);
+	}
+
+}
