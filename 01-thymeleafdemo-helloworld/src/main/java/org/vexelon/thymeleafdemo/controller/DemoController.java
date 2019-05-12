4348e457877d30a820057c6375a9907ba3e7697e
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2019-05-12 17:57:39 +0300

    Create HelloWorld Thymeleaf project

---
+++ 01-thymeleafdemo-helloworld/src/main/java/org/vexelon/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,22 @@
+package org.vexelon.thymeleafdemo.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+	// create a mapping for "/hello"
+
+	@GetMapping("/hello")
+	public String sayHello(Model theModel) {
+
+		theModel.addAttribute("theDate", new java.util.Date());
+
+		return "helloworld";
+	}
+
+}
